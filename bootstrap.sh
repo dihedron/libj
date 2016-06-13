80bc80fb1e5f1c4278435bac0353bdc2546145bf
Andrea Funtò <andrea.funto@bancaditalia.it>
2016-06-13 11:12:29 +0200

    removed sublime editor files (switching to VSCode)

--- bootstrap.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-./clean.sh
-cmake -G "Unix Makefiles"
--- clean.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-rm -rf CMakeCache.txt CMakeFiles/ cmake_install.cmake  Makefile runtime/CMakeCache.txt runtime/CMakeFiles/ runtime/cmake_install.cmake runtime/Makefile runtime/config.h driver
