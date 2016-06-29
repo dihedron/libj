dc4c4427a77cfa1fef4c84b98427876405777250
Andrea Funto <dihedron.dev@gmail.com>
2016-06-29 23:01:03 +0200

    added a todo to memleak

--- driver/memleak.h
+++ driver/memleak.h
@@ -15,11 +15,19 @@
 	void * operator new(size_t size);
 	void operator delete (void *p) noexcept(true);
 
+	// TODO: override all versions of new[] and delete[], taking into 
+	// account that some of them do not throw and return a null pointer 
+	// instead, plus provide logging versions of all C memory functions
+	// such as malloc calloc realloc and free via macros taking the __FILE__ 
+	// and __LINE__ parameters; store this information into an array
+	// so that any outstanding memory block can be logged when the 
+	// program exits (with some _atexit() or via an explicit call to 
+	// java::lang::System::gc() by the program)
+
 #else 
 	//
 	// memory leak detection not enabled
 	//
-
 
 #endif // DETECT_MEMLEAKS
 
